9ec4500bd6349c97c1ddbc98ebc2e50255bf5747
clayw <clayworks11337@gmail.com>
2025-10-22 20:26:38 +0400

    Change CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 
 int main() {
-	GrScImage<bool> img(100, 100, 0);
+	GrScImage<bool> img(1000, 1000, 0);
 	std::cout << "\n\n\n";
-	img.draw_circle(50, 50, 25, 1);
+	img.draw_circle(500, 500, 200, 1);
 
 	std::cout << img;
 
